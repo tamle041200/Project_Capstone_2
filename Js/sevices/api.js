e92900edbf38075392178e1ecc67eeec2d05ab77
tamle041200 <lehuutamcn2016@gmail.com>
2026-06-11 18:38:49 +0700

    update admin first

--- Js/sevices/api.js
+++ Js/sevices/api.js
@@ -1,10 +1,29 @@
 class Services {
   getListProductAPI() {
-    const url = "https://6a183c621878294b597ca1e4.mockapi.io/api/Product_phone";
+    const url = `https://6a183c621878294b597ca1e4.mockapi.io/api/Product_phone`;
 
     const promise = axios({
       url: url,
       method: "GET",
+    });
+    return promise;
+  }
+  deleteProductApi(id) {
+    const url = `https://6a183c621878294b597ca1e4.mockapi.io/api/Product_phone/${id}`;
+
+    const promise = axios({
+      url: url,
+      method: "DELETE",
+    });
+    return promise;
+  }
+  addProductApi(product) {
+    const url = `https://6a183c621878294b597ca1e4.mockapi.io/api/Product_phone`;
+
+    const promise = axios({
+      url: url,
+      method: "POST",
+      data: product,
     });
     return promise;
   }
